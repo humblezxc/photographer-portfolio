270a96adfbc391b075511de97b6a76a69a1d97f9
humblezxc <stopenchuk.yaroslav@gmail.com>
2023-08-11 23:40:02 +0200

    Deleted default styles

--- nextjs-portfolio/pages/index.jsx
+++ nextjs-portfolio/pages/index.jsx
@@ -14,11 +14,10 @@ export default function Portfolio() {
             <title>Photographer portfolio</title>
             <link rel="icon" href="/favicon.ico" />
         </Head>
-        {/*<header>*/}
-        {/*    <Navbar />*/}
-        {/*</header>*/}
-        <main>
+        <header>
             <Navbar />
+        </header>
+        <main>
             <Section id="home" title="Home">
                 <Home />
             </Section>
@@ -35,7 +34,7 @@ export default function Portfolio() {
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/humblezxc"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -43,65 +42,6 @@ export default function Portfolio() {
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        header {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
