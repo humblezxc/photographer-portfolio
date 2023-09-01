4c4b8e32f002d11ebab81583ae07f7ab0b819e17
humblezxc <stopenchuk.yaroslav@gmail.com>
2023-09-01 14:26:46 +0200

    Added tailwind and scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css'
+import './App.scss'
 // import Header from "./components/Header.tsx";
 // import Footer from "./components/Footer.tsx";
-import BeforeAfterSlider from "./components/BeforeAfterSlider.tsx";
+import BeforeAfterSliders from "./components/BeforeAfterSliders.tsx";
 
 function App() {
     return (
         <>
-            <BeforeAfterSlider />
+            <BeforeAfterSliders />
         </>
     );
 }
