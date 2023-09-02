7696ef8213d42da0845be61f231400f6d253e114
humblezxc <stopenchuk.yaroslav@gmail.com>
2023-09-02 17:21:26 +0200

    Updated BeforeAfterSliders

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './App.scss'
 // import Header from "./components/Header.tsx";
 // import Footer from "./components/Footer.tsx";
-import BeforeAfterSliders from "./components/BeforeAfterSliders.tsx";
+import BeforeAfterSliders from "./components/BeforeAfterSliders";
+import {presets} from "./data/presets";
 
 function App() {
     return (
-        <>
-            <BeforeAfterSliders />
-        </>
+        <div>
+            {presets.map(preset => <BeforeAfterSliders preset={preset} key={preset.id}/>)}
+        </div>
     );
 }
-
 export default App
--- src/components/BeforeAfterSliders.tsx
+++ src/components/BeforeAfterSliders.tsx
@@ -1,33 +1,22 @@
 import ReactBeforeSliderComponent from 'react-before-after-slider-component';
 import 'react-before-after-slider-component/dist/build.css';
+import {IPresets} from "../models.ts";
 
-export default function BeforeAfterSliders() {
-    const FIRST_IMAGE = {
-        imageUrl: 'src/assets/images/beforeAfter/img.jpg'
-    };
-    const SECOND_IMAGE = {
-        imageUrl: 'src/assets/images/beforeAfter/img_1.jpg'
-    };
-    const THIRD_IMAGE = {
-        imageUrl: 'src/assets/images/beforeAfter/img.png'
-    };
-    const FOUR_IMAGE = {
-        imageUrl: 'src/assets/images/beforeAfter/img_1.png'
-    };
-    const FIFTH_IMAGE = {
-        imageUrl: 'src/assets/images/beforeAfter/img_2.png'
-    };
-    const SIXTH_IMAGE = {
-        imageUrl: 'src/assets/images/beforeAfter/img_3.png'
-    };
+interface PresetsProps {
+    preset: IPresets
+}
+
+export default function BeforeAfterSliders({preset}: PresetsProps) {
+    console.log(preset.sliderImages)
+    preset.sliderImages.map((image) => console.log(image.beforeImg.imageUrl, image.afterImg.imageUrl))
 
     return (
         <main className="flex flex-col">
-            <h1 className="text-4xl mb-16">Sliders</h1>
+            <h1 className="text-4xl mb-16">{preset.title}</h1>
             <div className="flex flex-col p-2 lg:flex-row">
-                <ReactBeforeSliderComponent className="m-2" firstImage={FIRST_IMAGE} secondImage={SECOND_IMAGE}/>
-                <ReactBeforeSliderComponent className="m-2" firstImage={THIRD_IMAGE} secondImage={FOUR_IMAGE}/>
-                <ReactBeforeSliderComponent className="m-2" firstImage={FIFTH_IMAGE} secondImage={SIXTH_IMAGE}/>
+                {preset.sliderImages.map((image, index) => <ReactBeforeSliderComponent className="m-2" key={index}
+                                                                                       firstImage={image.beforeImg}
+                                                                                       secondImage={image.afterImg}/>)}
             </div>
             <button
                 className="mt-4 w-40 self-center rounded-full bg-white px-3 py-4 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0"
