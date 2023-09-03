9565a85296144064615de18d9c883f78a376cfc6
humblezxc <stopenchuk.yaroslav@gmail.com>
2023-09-04 01:59:27 +0200

    Updated Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
+import {Link} from "react-router-dom"
 export default function Header (){
     return(
-    <>
-        header
-    </>
+    <header>
+        <p><Link to="/">logo</Link></p>
+        <ul>
+            <li><Link to="/gallery">Gallery</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/presets">Presets</Link></li>
+        </ul>
+    </header>
     );
 }
