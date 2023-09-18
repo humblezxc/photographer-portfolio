d8e9face9ec6bbf3a33706604b3a909a20fb1d2a
humblezxc <stopenchuk.yaroslav@gmail.com>
2023-09-18 23:58:10 +0200

    Layout test

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
         <main>
-            <section className="container">
+            <section className="container flex-1">
                 <h1>Modal window</h1>
                 <p>Write me here:</p>
                 <ul>
@@ -9,6 +9,11 @@ export default function Contact() {
                     <li>telegram</li>
                     <li>mail</li>
                 </ul>
+                <div className="flex flex-col bg-yellow-400" style={{minHeight: "600px"}}>
+                    <div className="bg-green-400 -mt-20 self-center -z-1" style={{minHeight: "300px", maxWidth: "300px"}}>img</div>
+                    <p>layout test</p>
+                </div>
+
             </section>
         </main>
     );
