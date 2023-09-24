d430394d9d71bb751ac33f36612e4700fc420f2d
humblezxc <stopenchuk.yaroslav@gmail.com>
2023-09-24 23:51:16 +0200

    Added Contact info to Modal

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,19 +1,22 @@
+import {Modal} from "./Modal.tsx";
+
 export default function Contact() {
     return (
         <main>
             <section className="container flex-1">
-                <h1>Modal window</h1>
-                <p>Write me here:</p>
-                <ul>
-                    <li>Instagram</li>
-                    <li>telegram</li>
-                    <li>mail</li>
-                </ul>
-                <div className="flex flex-col bg-yellow-400" style={{minHeight: "600px"}}>
-                    <div className="bg-green-400 -mt-20 self-center -z-1" style={{minHeight: "300px", maxWidth: "300px"}}>img</div>
-                    <p>layout test</p>
-                </div>
-
+                <Modal title='Modal window'>
+                    <h1>Modal window</h1>
+                    <p>Write me here:</p>
+                    <ul>
+                        <li>Instagram</li>
+                        <li>telegram</li>
+                        <li>mail</li>
+                    </ul>
+                    <div className="flex flex-col bg-yellow-400" style={{minHeight: "600px"}}>
+                        <div className="bg-green-400 -mt-20 self-center -z-1" style={{minHeight: "300px", maxWidth: "300px"}}>img</div>
+                        <p>layout test</p>
+                    </div>
+                </Modal>
             </section>
         </main>
     );
