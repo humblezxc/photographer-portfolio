f9a770f0d21710d0ed895f01031d33c386fdb828
humblezxc <stopenchuk.yaroslav@gmail.com>
2023-11-23 00:14:35 +0100

    Fixed styles of Processing page

--- src/pages/Processing.tsx
+++ src/pages/Processing.tsx
@@ -2,18 +2,17 @@ import ReactCompareImage from "react-compare-image";
 import {processingImages} from "../data/processing.ts";
 
 export default function Processing() {
-    console.log(processingImages)
-
+    //TODO fix styles and zIndex when isModal
     return (
         <main>
             <section className="container pb-10" style={{position: "relative"}}>
-                <div className="flex flex-col gap-12 m-auto" style={{maxWidth: "500px", position: "absolute"}}>
-                    {processingImages.map(photos => (
-                            <ReactCompareImage key={photos.id}
-                                               leftImage={photos.image.beforeImage}
-                                               rightImage={photos.image.afterImage}
-                                               leftImageCss={{zIndex: 0}}
-                                               rightImageCss={{zIndex: 0}}
+                <div className="flex flex-col gap-12 m-auto" style={{maxWidth: "500px"}}>
+                    {processingImages.map(image => (
+                            <ReactCompareImage key={image.id}
+                                               leftImage={image.image.beforeImage}
+                                               rightImage={image.image.afterImage}
+                                               // leftImageCss={{zIndex: 0}}
+                                               // rightImageCss={{zIndex: 0}}
                             />
                     ))}
                 </div>
